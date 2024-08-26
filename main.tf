34f671553b0dcb8bd74d2e75a4161ef123cda044
Gangadharam-github <gangadharamchalla66@gmail.com>
2024-08-26 10:23:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "ap-south-01"
+
 }
 
 resource "aws_instance" "dev" {
   ami = "ami-0a4408457f9a03be3"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "God"
 
   tags = {
